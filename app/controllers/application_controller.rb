1c2f3f70016abb25287fd432e9963b4c5cf066cf
nhkhanh68 <nhkhanh68@gmail.com>
2017-02-09 09:54:41 +0700

    end
    end
    
    end
    
    end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,6 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
     Rails.application.routes.default_url_options[:locale]= I18n.locale
   end
+
+  include SessionsHelper
 end
